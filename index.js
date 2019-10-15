f234375ffaf50ddbeee3cc30687e39ba4abab7c8
Alexander Federlin <alexander@federlin.de>
2019-10-15 20:32:12 +0100

    initial commit of express based app using canvas.js for graphing values read from serial port

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+const SerialPort = require('serialport')
+const Readline = SerialPort.parsers.Readline
+//const port = new SerialPort(path)
+const port = new SerialPort('/dev/ttyACM0')
+const parser = new Readline()
+port.pipe(parser)
+
+parser.on('data', function (data) {
+  console.log('Data:', data)
+  io.emit('data', data)
+})
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
